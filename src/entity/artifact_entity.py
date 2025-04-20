39bf7219eee426f1f29fe207cfbe17e016fae3e4
Divyanshu Rajput <dvnhrajput@gmail.com>
2025-04-20 10:57:13 +0530

    DataValidation Module and training pipeline portion complete

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -5,5 +5,11 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionArtifact:
     trained_file_path: str
-    test_file_path: str
+    tested_file_path: str
+    
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    message: str
+    validation_report_file_path: str
     
