66d8db543e6650977c213c10bb57524f86a9f226
Divyanshu Rajput <dvnhrajput@gmail.com>
2025-04-23 14:12:59 +0530

    Prediction Pipeline completed and app is running

--- src/cloud_storage/aws_storage.py
+++ src/cloud_storage/aws_storage.py
@@ -58,12 +58,12 @@ class SimpleStorageService:
         try:
             # Read and decode content if decode=True
             func = (
-                lambda: object_name.get()['body'].read().decode() if decode else object_name.get()['body'].read()
+                lambda: object_name.get()['Body'].read().decode() if decode else object_name.get()['Body'].read()
             )
             
             # Convert content into readable form if makek_readable=True
             conv_func = lambda: StringIO(func()) if make_readable else func()
-            return conv_func
+            return conv_func()
         except Exception as e:
             raise MyException(e, sys)
         
@@ -123,6 +123,7 @@ class SimpleStorageService:
             model_file = model_dir + "/" + model_name if model_dir else model_name
             file_object = self.get_file_object(model_file, bucket_name)
             model_obj = self.read_object(file_object, decode=False)
+            print(type(model_obj))
             model = pickle.loads(model_obj)
             logging.info("Production model loaded from S3 bucket.")
             return model
